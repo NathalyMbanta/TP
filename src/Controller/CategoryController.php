9c9551fe8406282d53dfbe13baea74d0dc0e7ffc
nathalymbanta <nathalytshika@gmail.com>
2022-08-12 17:05:34 +0200

    tp

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\AsciiSlugger;
@@ -12,11 +15,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'category')]
-    public function index(CategoryRepository $categroryRepository ): Response
+    public function index(CategoryRepository $categoryRepository ): Response
     { 
-        $category=$categroryRepository->findAll();
+        $categorys=$categoryRepository->findAll();
         return $this->render('category/index.html.twig', [
-            'category' => $category,
+            'categorys' => $categorys,
         ]);
     }
 
@@ -25,33 +28,31 @@ class CategoryController extends AbstractController
     {
          $product = $categoryRepository->findOneBy(['slug' => $slug]);
          return $this->render('category/category_Show.html.twig', [
-             'category' => $product
+             'category' => $category,
          ]);
      }
 
     #[Route('/admin/category', name: 'admin_category')]
-    public function adminList(CategoryRepository $CategryRepository): Response
+    public function adminList(CategoryRepository $categoryRepository): Response
     {
-        $category = $productRepository->findAll();
+        $categorys = $categoryRepository->findAll();
         return $this->render('category/adminList_Category.html.twig', [
             'categorys' => $categorys
         ]);
     }
 
     #[Route('/admin/category/create', name: 'category_create')]
-
-    public function create(Request $request, CategoryRepository $categegoryRepository, ManagerRegistry $managerRegistry): Response
+    public function create(Request $request, CategoryRepository $categoryRepository, ManagerRegistry $managerRegistry): Response
     {
         $category = new Category(); // création d'une nouvelle category
         $form = $this->createForm(CategoryType::class, $category); // création d'un formulaire avec en paramètre nouvelle category
         $form->handleRequest($request); // gestionnaire de requêtes HTTP
 
-
         if ($form->isSubmitted() && $form->isValid()) { // vérifie si le formulaire a été soumis et est valide
 
-            $category = $categoryRepository->findAll(); // récupère tous les category en base de données
+            $categorys = $categoryRepository->findAll(); // récupère tous les category en base de données
             $categoryNames = []; // initialise un tableau pour les noms de category
-            foreach ($categorys as $category) { // pour chaque produit récupéré
+            foreach ($categorys as $category) { // pour chaque categorie récupéré
                 $categoryNames[] = $category->getName(); // stocke le nom de la categorie dans le tableau
             }
             if (in_array($form['name']->getData(), $categoryNames)) { // vérifie qsi le nom de la categorie à créé n'est pas déjà utilisé en base de données
@@ -59,18 +60,18 @@ class CategoryController extends AbstractController
                 return $this->redirectToRoute('admin_category');
             }
 
-                $infoImg = $form['img']->getData(); // récupère les informations de l'image dans le formulaire
-                if ($infoImg !== null) { // s'il y a bien une image donnée dans le formulaire
-                    $oldImgName = $product->getImg(); // récupère le nom de l'ancienne image
-                    $oldImgPath = $this->getParameter('category_image_dir') . '/' . $oldImgName; // récupère le chemin de l'ancienne image 
-                    if (file_exists($oldImg1Path)) {
-                        unlink($oldImg1Path); // supprime l'ancienne image 
-                    }
-                    $extensionImg = $infoImg->guessExtension(); // récupère l'extension de fichier de l'image 
-                    $nomImg = time() . '-1.' . $extensionImg; // crée un nom de fichier unique pour l'image 
-                    $infoImg->move($this->getParameter('category_image_dir'), $nomImg); // télécharge le fichier dans le dossier adéquat
-                    $category->setImg1($nomImg); // définit le nom de l'image à mettre en base de données
+            $infoImg = $form['img']->getData(); // récupère les informations de l'image dans le formulaire
+            if ($infoImg !== null) { // s'il y a bien une image donnée dans le formulaire
+                $oldImgName = $category->getImg(); // récupère le nom de l'ancienne image
+                $oldImgPath = $this->getParameter('category_image_dir') . '/' . $oldImgName; // récupère le chemin de l'ancienne image 
+                if (file_exists($oldImgPath)) {
+                    unlink($oldImgPath); // supprime l'ancienne image 
                 }
+                $extensionImg = $infoImg->guessExtension(); // récupère l'extension de fichier de l'image 
+                $nomImg = time() . '-1.' . $extensionImg; // crée un nom de fichier unique pour l'image 
+                $infoImg->move($this->getParameter('category_image_dir'), $nomImg); // télécharge le fichier dans le dossier adéquat
+                $category->setImg1($nomImg); // définit le nom de l'image à mettre en base de données
+            }
 
             $slugger = new AsciiSlugger();
             $category->setSlug(strtolower($slugger->slug($form['name']->getData()))); // génère un slug à partir du titre renseigné dans le formulaire
@@ -83,31 +84,80 @@ class CategoryController extends AbstractController
             $this->addFlash('success', 'La categorie a été crée'); // message de succès
             return $this->redirectToRoute('admin_category');
         }
-            return $this->render('category/form.html.twig', [
-                'productForm' => $form->createView()
-            ]);
 
-            } 
+        return $this->render('category/form.html.twig', [
+            'categoryForm' => $form->createView()
+        ]);
+
+    }
 
 
-            #[Route('/admin/category/delete/{id}', name: 'category_delete')]
-            public function delete(Category $product, ManagerRegistry $managerRegistry): Response
-            {
-                $imgpath = $this->getParameter('category_image_dir') . '/' . $product->getImg();
-                if (file_exists($imgpath)) {
-                    unlink($imgpath);
-                }
-        
-        
-                $manager = $managerRegistry->getManager();
-                $manager->remove($product);
-                $manager->flush();
-        
-                $this->addFlash('success', 'Le produit a bein été supprimé');
+    #[Route('/admin/category/delete/{id}', name: 'category_delete')]
+    public function delete(Category $product, ManagerRegistry $managerRegistry): Response
+    {
+        $imgpath = $this->getParameter('category_image_dir') . '/' . $product->getImg();
+        if (file_exists($imgpath)) {
+            unlink($imgpath);
+        }
+
+
+        $manager = $managerRegistry->getManager();
+        $manager->remove($product);
+        $manager->flush();
+
+        $this->addFlash('success', 'La category a bein été supprimé');
+        return $this->redirectToRoute('admin_category');
+    }
+
+
+    #[Route('/admin/category/update/{id}', name: 'category_update')]
+    public function update(Category $category, CategoryRepository $categoryRepository, Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $form = $this->createForm(CategoryType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $categorys = $productRepository->findAll(); // récupère tous les categorie en base de données
+            $categoryNames = []; // initialise un tableau pour les noms de categorie
+            foreach ($categorys as $category) { // pour chaque categorie récupéré
+                $categoryNames[] = $category->getName(); // stocke le nom de categorie  dans le tableau
+            }   
+            if (in_array($form['name']->getData(), $categoryNames)) { // vérifie qsi le nom de la categorie à créé n'est pas déjà utilisé en base de données
+                $this->addFlash('danger', 'La categorie n\'a pas pu être modifié : le nom de produit est déjà utilisé');
                 return $this->redirectToRoute('admin_category');
             }
-            
-           
-            
+
+            $infoImg = $form['img']->getData(); // récupère les informations de l'image 1 dans le formulaire
+            if ($infoImg !== null) { // s'il y a bien une image donnée dans le formulaire
+                $oldImgName = $category->getImg1(); // récupère le nom de l'ancienne image
+                $oldImgPath = $this->getParameter('product_image_dir') . '/' . $oldImgName; // récupère le chemin de l'ancienne image 1
+                if (file_exists($oldImgPath)) {
+                    unlink($oldImgPath); // supprime l'ancienne image 
+                }
+                $extensionImg = $infoImg->guessExtension(); // récupère l'extension de fichier de l'image 
+                $nomImg = time() . '-1.' . $extensionImg; // crée un nom de fichier unique pour l'image 
+                $infoImg->move($this->getParameter('category_image_dir'), $nomImg); // télécharge le fichier dans le dossier adéquat
+                $category->setImg1($nomImg); // définit le nom de l'image à mettre ne base de données
+            }
+
+          
+        
+
+                $slugger = new AsciiSlugger();
+                $category->setSlug(strtolower($slugger->slug($form['name']->getData())));
+                $manager = $managerRegistry->getManager();
+                $manager->persist($category);
+                $manager->flush();
+    
+                $this->addFlash('success', 'La category a bien été modifié');
+                return $this->redirectToRoute('admi_category');
         }
+    
+            return $this->render('category/form.html.twig', [
+                'categoryForm' => $form->createView()
+            ]);
+        
             
+}
+}
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,9 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CategoryType extends AbstractType
 {
